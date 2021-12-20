339a48164198dc6829f48eeb39b1512993a6622e
“Hassan <mian@willandskill.se>
2021-12-20 12:45:14 +0100

    Add interest and relationship selection

--- src/pages/InterestSelectionPage.jsx
+++ src/pages/InterestSelectionPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { SelectionContext } from '../App'
+import CustomLink from '../components/CustomLink'
+import Heading2 from '../components/Heading2'
+import RadioButton from '../components/RadioButton'
 
 export default function InterestSelectionPage() {
+    const {setInterest} = useContext(SelectionContext)
+    
+    function handleOnChange(e) {
+        setInterest(e.target.value)
+    }
+
     return (
         <div>
-            Interest Selection Page
+            <Heading2 text="Select Age" />
+            <RadioButton 
+                text="Fashion"
+                value="a" 
+                name="age" 
+                onChange={handleOnChange}
+            />
+            <RadioButton 
+                text="Tech"
+                value="b" 
+                name="age" 
+                onChange={handleOnChange}
+            />
+            <RadioButton 
+                text="Craft"
+                value="c" 
+                name="age" 
+                onChange={handleOnChange}
+            />
+            <CustomLink to="/relationship" text="Lets choose relationship"/>
         </div>
     )
 }
--- src/pages/RelationshipSelectionPage.jsx
+++ src/pages/RelationshipSelectionPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { SelectionContext } from '../App'
+import CustomLink from '../components/CustomLink'
+import Heading2 from '../components/Heading2'
+import RadioButton from '../components/RadioButton'
 
 export default function RelationshipSelectionPage() {
+    const {setRelationship} = useContext(SelectionContext)
+    
+    function handleOnChange(e) {
+        setRelationship(e.target.value)
+    }
+
     return (
         <div>
-            Relationship Selection Page
+            <Heading2 text="Select Age" />
+            <RadioButton 
+                text="Parent"
+                value="a" 
+                name="age" 
+                onChange={handleOnChange}
+            />
+            <RadioButton 
+                text="Sibling"
+                value="b" 
+                name="age" 
+                onChange={handleOnChange}
+            />
+            <RadioButton 
+                text="Other"
+                value="c" 
+                name="age" 
+                onChange={handleOnChange}
+            />
+            <CustomLink to="/result" text="Lets choose relationship"/>
         </div>
     )
 }
